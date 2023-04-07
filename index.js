89750f3a4e98ff72278d2d14816b1f87b9dce3a5
yebin <sesa524903@se.com>
2023-04-07 14:45:32 +0800

    test

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ AppRegistry.registerComponent(appName, () => App);
 
 class App extends Component {
 
+    getVersion() {
+        return isConnected()?'1':'0'
+    }
+
     render() {
         return (
             <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
                 <Text>RN-0.71-hello sonar tow branch</Text>
                 <Text>add three branch hello</Text>
+                <Text>{'version:'+this.getVersion()}</Text>
             </View>
         )
     }
